07dd0a2cb6801938dfbba7f7a19cd34a10c6d3e0
Andrew Smodlev <asmodlev@kameleoon.com>
2021-09-23 12:53:23 +0300

    wip

--- src/game/level.ts
+++ src/game/level.ts
@@ -64,6 +64,7 @@ export class Level {
 		dx: 0,
 		dy: 0
 	  };
+	private bricks: Brick[] =[];
 
 
 	constructor() {
@@ -80,22 +81,20 @@ export class Level {
 		this.canvas.width = this.canvasConfig.width;
 		this.canvas.height = this.canvasConfig.height;
 
-		if (!this.ctx) return;
 		this.ctx.fillStyle = this.canvasConfig.borderColor;
 		this.ctx.fillRect(0, 0, this.canvas.width, 12);
 		this.ctx.fillRect(0, 0, 12, this.canvas.height);
 		this.ctx.fillRect(this.canvas.width - 12, 0, 12, this.canvas.height);
 	}
 
-	private setBricksOnCanvas() {
-		const bricks: Brick[] = [];
+	private setBricks() {
 		const {brickGap, brickHeight, brickWidth} = this.brickConfig;
 		for (let row = 0; row < this.map.length; row++) {
 			for (let col = 0; col < this.map[row]!.length; col++) {
 		  
 			  const colorCode = this.map[row]![col]! as keyof ColorsMap;
 		  
-			  bricks.push({
+			  this.bricks.push({
 				x: this.wallSize + (brickWidth + brickGap) * col,
 				y: this.wallSize + (brickHeight + brickGap) * row,
 				color: this.colorMap[colorCode],
@@ -105,20 +104,7 @@ export class Level {
 			}
 	  	}
 
-		bricks.forEach(brick => {
-			this.ctx.fillStyle = brick.color;
-			this.ctx?.fillRect(brick.x, brick.y, brick.width, brick.height);
-		});
-		  
 	}
-
-	// from mozilla gams
-	private collides(ball: Ball, paddle: Paddle | Brick) {
-		return ball.x < paddle.x + paddle.width &&
-			   ball.x + ball.width > paddle.x &&
-			   ball.y < paddle.y + paddle.height &&
-			   ball.y + ball.height > paddle.y;
-    }
 
     static loadImgForPaddle():HTMLImageElement {
 	    const image = document.createElement('img');
@@ -155,6 +141,61 @@ export class Level {
 		  });
 	}
 
+	private manipulateBallPhysics() {
+		if (this.ball.x < this.wallSize) {
+			this.ball.x = this.wallSize;
+			this.ball.dx *= -1;
+		}
+		else if (this.ball.x + this.ball.width > this.canvas.width - this.wallSize) {
+			this.ball.x = this.canvas.width - this.wallSize - this.ball.width;
+			this.ball.dx *= -1;
+		}
+
+		if (this.ball.y < this.wallSize) {
+			this.ball.y = this.wallSize;
+			this.ball.dy *= -1;
+		}
+
+		if (this.ball.y > this.canvas.height) {
+			this.ball.x = 130;
+			this.ball.y = 260;
+			this.ball.dx = 0;
+			this.ball.dy = 0;
+		}
+	}
+
+	private manipulateBrakingBlocks() {
+		const collides = (ball: Ball, paddle: Paddle | Brick) => {
+			return ball.x < paddle.x + paddle.width &&
+				ball.x + ball.width > paddle.x &&
+				ball.y < paddle.y + paddle.height &&
+				ball.y + ball.height > paddle.y;
+		}
+
+		if (collides(this.ball, this.paddle)) {
+			this.ball.dy *= -1;
+			this.ball.y = this.paddle.y - this.ball.height;
+		}
+
+		for(let index = 0; index < this.bricks.length; index++)  {
+			const brick = this.bricks[index];
+			if (collides(this.ball, brick)) {
+
+				this.bricks.splice(index, 1);
+
+				// collides from top or bott
+				if (this.ball.y + this.ball.height - this.ball.speed <= brick.y ||
+					this.ball.y >= brick.y + brick.height - this.ball.speed) {
+					this.ball.dy *= -1;
+				} else {
+					this.ball.dx *= -1;
+				}
+
+				break;
+			}
+		}
+	}
+
 	private renderLevel() {
 
 		requestAnimationFrame(() => this.renderLevel());
@@ -166,14 +207,39 @@ export class Level {
 		this.paddle.x += this.paddle.dx;
 		if (this.paddle.x < this.wallSize) {
 			this.paddle.x = this.wallSize
-		}
-		 else if (this.paddle.x + this.brickConfig.brickWidth > this.canvas.width - this.wallSize) {
+		} else if (this.paddle.x + this.brickConfig.brickWidth > this.canvas.width - this.wallSize) {
 			this.paddle.x = this.canvas.width - this.wallSize - this.brickConfig.brickWidth;
 		}
 
-		 //draw paddle
+		//move ball
+		this.ball.x += this.ball.dx;
+		this.ball.y += this.ball.dy;
+
+		this.manipulateBallPhysics();
+		this.manipulateBrakingBlocks();
+
+		//draw paddle
 		this.ctx.drawImage(this.paddle.img, this.paddle.x, this.paddle.y);
 
+		//draw ball
+		if (this.ball.dx || this.ball.dy) {
+			//TODO: add ball img or color
+			this.ctx.fillStyle = 'yellow';
+			this.ctx.fillRect(this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+		}
+
+		//draw bricks
+		this.bricks.forEach(brick => {
+			this.ctx.fillStyle = brick.color;
+			this.ctx?.fillRect(brick.x, brick.y, brick.width, brick.height);
+		});
+
+
+		//draw the walls
+		this.ctx.fillStyle = this.canvasConfig.borderColor;
+		this.ctx.fillRect(0, 0, this.canvas.width, 12);
+		this.ctx.fillRect(0, 0, 12, this.canvas.height);
+		this.ctx.fillRect(this.canvas.width - 12, 0, 12, this.canvas.height);
 	}	
 
 	private init() {
@@ -184,7 +250,7 @@ export class Level {
 		container.appendChild(this.canvas);
 
 		this.setBasicCanvasConfig();
-		this.setBricksOnCanvas();
+		this.setBricks();
 		this.setListeners();
 		requestAnimationFrame(() => this.renderLevel());
 	}
